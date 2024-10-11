512240a0bced183be49bdcf8513827e39e934103
Anh Tuan <thongocnguyen1011@gmail.com>
2024-10-12 00:34:29 +0700

    refuse to select name and quotes

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -1,3 +1,5 @@
 export default function Name() {
-  return <div className="text-3xl font-semibold">Hoàng Anh Tuấn</div>;
+  return (
+    <div className="select-none text-3xl font-semibold">Hoàng Anh Tuấn</div>
+  );
 }
