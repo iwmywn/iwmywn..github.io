eb4061eb635131955f566e4135727f142bddfe60
Anh Tuan <thongocnguyen1011@gmail.com>
2024-10-14 00:15:31 +0700

    move ripple effect out of main

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,14 @@ export default function App() {
   const ripples = useRippleEffect();
 
   return (
-    <main className="flex min-h-screen animate-color-change flex-col items-center justify-center gap-y-4 text-white">
+    <>
+      <main className="flex min-h-screen animate-color-change flex-col items-center justify-center gap-y-4 text-white">
+        <MemoizedInfo />
+        <MemoizedRandomQuote />
+      </main>
       {ripples.map((ripple) => (
         <RippleEffect key={ripple.id} x={ripple.x} y={ripple.y} />
       ))}
-      <MemoizedInfo />
-      <MemoizedRandomQuote />
-    </main>
+    </>
   );
 }
