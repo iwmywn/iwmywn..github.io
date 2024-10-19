2472e86a42082909ae2a62a5d942f842867f1204
Anh Tuan <thongocnguyen1011@gmail.com>
2024-10-19 14:28:42 +0700

    add labels to links

--- src/components/LinkItem.tsx
+++ src/components/LinkItem.tsx
@@ -1,17 +1,19 @@
 import { ReactNode } from "react";
 
 interface LinkProps {
-  children: ReactNode;
   to: string;
+  label: string;
+  children: ReactNode;
 }
 
-export default function LinkItem({ to, children }: LinkProps) {
+export default function LinkItem({ to, label, children }: LinkProps) {
   return (
     <li className="flex h-9 w-9 items-center justify-center">
       <a
         href={to}
         target="_blank"
         rel="noopener"
+        aria-label={label}
         className="transition-all duration-300 hover:scale-125"
       >
         {children}
--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -5,16 +5,22 @@ import LinkItem from "./LinkItem";
 export default function Links() {
   return (
     <ul className="flex flex-nowrap gap-x-5">
-      <LinkItem to="https://github.com/iwmywn">
+      <LinkItem to="https://github.com/iwmywn" label="Visit GitHub profile">
         <FaGithub fontSize={25} />
       </LinkItem>
-      <LinkItem to="https://www.facebook.com/iwmy.wn">
+      <LinkItem
+        to="https://www.facebook.com/iwmy.wn"
+        label="Visit Facebook profile"
+      >
         <FaFacebook fontSize={25} />
       </LinkItem>
-      <LinkItem to="https://www.instagram.com/iwmy_wn">
+      <LinkItem
+        to="https://www.instagram.com/iwmy_wn"
+        label="Visit Instagram profile"
+      >
         <FaInstagram fontSize={25} />
       </LinkItem>
-      <LinkItem to="mailto:itsmethu2408@gmail.com">
+      <LinkItem to="mailto:itsmethu2408@gmail.com" label="Send an email">
         <IoMdMail fontSize={25} />
       </LinkItem>
     </ul>
