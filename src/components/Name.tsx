5cea1a5770ca065fb8390d15dabaa909a38c2fba
Anh Tuan <thongocnguyen1011@gmail.com>
2024-10-20 12:24:12 +0700

    use span tag for name

--- src/components/Name.tsx
+++ src/components/Name.tsx
@@ -1,7 +1,7 @@
 export default function Name() {
   return (
-    <div className="select-none whitespace-nowrap text-3xl font-semibold">
+    <span className="select-none whitespace-nowrap text-3xl font-semibold">
       Hoàng Anh Tuấn
-    </div>
+    </span>
   );
 }
