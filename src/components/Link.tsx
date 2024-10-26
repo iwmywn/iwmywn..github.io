d05930924710e9bb7f1d857e6a0424b05df0874c
Anh Tuan <thongocnguyen1011@gmail.com>
2024-10-26 18:56:44 +0700

    change font size

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -14,7 +14,7 @@ export default function Link({ to, label, children }: LinkProps) {
         target="_blank"
         rel="noopener"
         aria-label={label}
-        className="text-2xl transition-all duration-300 hover:scale-125"
+        className="text-[1.625rem] transition-all duration-300 hover:scale-125"
       >
         {children}
       </a>
