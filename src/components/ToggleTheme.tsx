13a89c72c0ad716beead77ed9fe5234c9b2d0d64
Anh Tuan <thongocnguyen1011@gmail.com>
2024-10-30 10:58:55 +0700

    save theme value ​​to local storage

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -2,10 +2,15 @@ import { useEffect, useState } from "react";
 import toggleDark from "../features/theme/toggleDark";
 
 export default function ToggleTheme() {
-  const [isDark, setIsDark] = useState<boolean>(true);
+  const [isDark, setIsDark] = useState<boolean>(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme === "dark" || savedTheme === null;
+  });
 
   useEffect(() => {
     document.documentElement.classList.toggle("dark", isDark);
+
+    localStorage.setItem("theme", isDark ? "dark" : "light");
   }, [isDark]);
 
   useEffect(() => {
