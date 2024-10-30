9a59ca6d4363b4e8c9f98d3e7aa9e98d4d685215
Anh Tuan <thongocnguyen1011@gmail.com>
2024-10-30 16:51:14 +0700

    text color for body instead of main

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import ToggleTheme from "./components/ToggleTheme";
 export default function App() {
   return (
     <>
-      <main className="animate-color-change-light dark:animate-color-change-dark flex min-h-screen flex-col items-center justify-center gap-y-4 text-black dark:text-white">
+      <main className="flex min-h-screen animate-color-change-light flex-col items-center justify-center gap-y-4 dark:animate-color-change-dark">
         <Info />
         <RandomQuote />
       </main>
