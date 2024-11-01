9e712075e49c48792ef376854e0e1590cea371bb
Anh Tuan <thongocnguyen1011@gmail.com>
2024-11-01 13:04:55 +0700

    no need to link component no more

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,6 +1,5 @@
 import { FaGithub, FaFacebook, FaInstagram } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
-import Link from "./Link";
 import socialLinks from "../data/socialLinks";
 
 const iconMap = {
@@ -14,10 +13,16 @@ export default function Links() {
   return (
     <ul className="flex flex-nowrap gap-x-5">
       {socialLinks.map(({ href, label, iconName }, index) => (
-        <li className="flex h-9 w-9 items-center justify-center">
-          <Link key={index} to={href} label={label}>
+        <li key={index} className="flex h-9 w-9 items-center justify-center">
+          <a
+            href={href}
+            target="_blank"
+            rel="noopener"
+            aria-label={label}
+            className="text-[1.625rem] transition-all duration-300 hover:scale-125"
+          >
             {iconMap[iconName]}
-          </Link>
+          </a>
         </li>
       ))}
     </ul>
