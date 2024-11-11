809e9ce81265b1eccbe6a8c266d79e84970168fb
Anh Tuan <thongocnguyen1011@gmail.com>
2024-11-11 20:01:57 +0700

    merge name with links into info

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,11 +1,33 @@
-import Name from "./Name";
-import Links from "./Links";
+import socialLinks from "../data/socialLinks";
 
 export default function Info() {
   return (
     <div className="flex flex-col items-center justify-center gap-y-4">
-      <Name />
-      <Links />
+      <span className="select-none whitespace-nowrap text-3xl font-bold">
+        Hoàng Anh Tuấn
+      </span>
+
+      <ul className="flex flex-nowrap gap-x-5">
+        {socialLinks.map(({ href, label, icon }, index) => {
+          const LinkIcon = icon;
+          return (
+            <li
+              key={index}
+              className="flex h-9 w-9 items-center justify-center"
+            >
+              <a
+                className="text-[1.625rem] transition-all duration-300 hover:scale-125"
+                href={href}
+                target="_blank"
+                rel="noopener"
+                aria-label={label}
+              >
+                <LinkIcon />
+              </a>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
--- src/components/Name.tsx
+++
@@ -1,7 +0,0 @@
-export default function Name() {
-  return (
-    <span className="select-none whitespace-nowrap text-3xl font-bold">
-      Hoàng Anh Tuấn
-    </span>
-  );
-}
